ce7759f564828fdb8f8a656c4a4c06a6bd25ee99
U Akhila <uakhila908@gmail.com>
2026-09-09 20:42:11 +0530

    feat: add benchmark_cold_target_study module alias and re-exports

---
+++ src/training/__init__.py
@@ -0,0 +1,3 @@
+"""Training and benchmark suites for AuditDDI."""
+
+from src.training.benchmark_cold_target import run_cold_target_study  # noqa: F401
---
+++ src/training/benchmark_cold_target_study.py
@@ -0,0 +1,4 @@
+"""Convenience alias module for benchmark_cold_target."""
+
+from src.training.benchmark_cold_target import *  # noqa: F401, F403
+from src.training.benchmark_cold_target import run_cold_target_study  # noqa: F401
